72b8621721599320bcc0ea8a82bd21183101a8a9
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2024-11-05 20:15:07 +0530

    Create WAP_override.cpp

---
+++ OOPS/WAP_override.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+// Base class: Vehicle
+class Vehicle {
+public:
+    // Virtual method
+    virtual void display() {
+        cout << "This is a vehicle." << endl;
+    }
+};
+
+// Derived class: Car
+class Car : public Vehicle {
+public:
+    // Overriding the display method
+    void display() override {
+        cout << "This is a car." << endl;
+    }
+};
+
+int main() {
+    Vehicle* vehicle;
+    Car car;
+
+    // Base class pointer pointing to a Car object
+    vehicle = &car;
+    vehicle->display();  // Calls Car's overridden method
+
+    return 0;
+}
