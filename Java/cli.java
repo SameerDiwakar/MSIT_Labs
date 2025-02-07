37ffc9d7f97dbf4ee18042912c50e50ecaf8f83a
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2025-02-07 15:12:34 +0530

    Create cli.java

---
+++ Java/cli.java
@@ -0,0 +1,9 @@
+public class cli{
+ public static void main(String args[]){
+	for (int i = 0; i < args.length;i++){
+		System.out.println("Command line argument " + i +":" + args[i]);
+	
+	}
+
+}
+};
