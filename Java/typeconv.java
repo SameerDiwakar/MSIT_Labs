6d4cb0a74d7ac3180d1084e28c8b0618c32de688
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2025-02-14 15:49:38 +0530

    Update typeconv.java

--- Java/typeconv.java
+++ Java/typeconv.java
@@ -2,8 +2,10 @@ public class typeconv{
 	public static void main(String[] args){
 		int a = 2;
 		double e = a;
-		int c = e * 22;
-		System.out.println(a + "   " + e + "  " + c);
+		int c = (int)e*22;
+		System.out.println("Initial integer "+a);
+		System.out.println("Converted to double  "+e);
+		System.out.println("Double  integer "+c);
 
 
 }
