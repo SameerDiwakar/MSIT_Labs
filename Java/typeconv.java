f062405ca4ece702a633ff6ce9457760d3929323
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2025-02-14 15:41:49 +0530

    Create typeconv.java

---
+++ Java/typeconv.java
@@ -0,0 +1,18 @@
+public class typeconv{
+	public static void main(String[] args){
+		int a = 2;
+		double e = a;
+		int c = e * 22;
+		System.out.println(a + "   " + e + "  " + c);
+
+
+}
+
+
+
+
+
+
+
+
+}
