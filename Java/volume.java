79ebbbe3b6208445d30f78e27a6bc1c2f27326fa
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2025-02-14 15:43:17 +0530

    Create volume.java

---
+++ Java/volume.java
@@ -0,0 +1,17 @@
+public class vol{
+	public static void main(String[] args){
+		int r = Integer.parseInt(args[0]);
+		int h = Integer.parseInt(args[1]);
+      		System.out.println("Volume of cylinder is " + 3.14*r*r*h);
+
+
+}
+
+
+
+
+
+
+
+
+}
