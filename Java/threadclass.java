1b3aede854812184f4992dbd453ed261d86473f7
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2025-05-19 07:08:42 +0530

    Update threadclass.java

--- Java/threadclass.java
+++ Java/threadclass.java
@@ -10,3 +10,33 @@ public class Main {
         t1.start();
     }
 }
+
+class MyTask extends Thread { 
+    String taskName;
+
+    MyTask(String name) {
+        this.taskName = name;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(taskName + " - Count: " + i);
+            try {
+                Thread.sleep(500);  
+            } catch (InterruptedException e) {
+                System.out.println(taskName + " interrupted");
+            }
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MyTask t1 = new MyTask("Thread A");
+        MyTask t2 = new MyTask("Thread B");
+
+        t1.start();
+        t2.start();
+    }
+}
