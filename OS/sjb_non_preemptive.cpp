590167814db70d3724c587ac9e8e256e1c133c78
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2025-08-13 13:47:50 +0530

    Create sjb_non_preemptive.cpp

---
+++ OS/sjb_non_preemptive.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+class Process {
+public:
+    int pid;
+    int atime;
+    int btime;
+
+    Process(int pid, int atime, int btime) {
+        this->pid = pid;
+        this->atime = atime;
+        this->btime = btime;
+    }
+};
+
+void fcfs(vector<Process> &processes) {
+    // Sort by arrival time
+    sort(processes.begin(), processes.end(), [](Process a, Process b) {
+        return a.btime < b.btime;
+    });
+
+    cout << "Process ID  Arrival Time   Burst Time     Waiting Time   Turnaround Time" << endl;
+    cout << "----------------------------------------------------------------------" << endl;
+
+    int totaltime = 0;
+    for (auto &process : processes) {
+        int waiting_time = totaltime - process.atime;
+        if (waiting_time < 0) waiting_time = 0;
+        totaltime += process.btime;
+        int turnaround_time = totaltime - process.atime;
+
+        cout << setw(8) << process.pid << "   "
+             << setw(10) << process.atime << "   "
+             << setw(10) << process.btime << "   "
+             << setw(12) << waiting_time << "   "
+             << setw(12) << turnaround_time << endl;
+    }
+}
+
+int main() {
+    vector<Process> processes = {
+        Process(1, 0, 5),
+        Process(2, 1, 7),
+        Process(3, 2, 6),
+        Process(4, 3, 9)
+    };
+    fcfs(processes);
+    return 0;
+}
