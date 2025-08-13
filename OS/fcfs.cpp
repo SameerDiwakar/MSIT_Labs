1171f54a7f1abf1c163ecaef54f2d639cb69ff72
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2025-08-13 13:38:19 +0530

    improved output

--- OS/fcfs.cpp
+++ OS/fcfs.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <iomanip>
 
 using namespace std;
 
@@ -23,22 +24,30 @@ void fcfs(vector<Process> &processes) {
         return a.atime < b.atime;
     });
 
+    cout << "Process ID  Arrival Time   Burst Time     Waiting Time   Turnaround Time" << endl;
+    cout << "----------------------------------------------------------------------" << endl;
+
     int totaltime = 0;
     for (auto &process : processes) {
+        int waiting_time = totaltime - process.atime;
+        if (waiting_time < 0) waiting_time = 0;
         totaltime += process.btime;
-        cout << "Process ID: " << process.pid
-             << " Arrival Time: " << process.atime
-             << " Burst Time: " << process.btime
-             << " Waiting Time: " << totaltime - process.btime - process.atime
-             << " Turnaround Time: " << totaltime - process.atime << endl;
+        int turnaround_time = totaltime - process.atime;
+
+        cout << setw(8) << process.pid << "   "
+             << setw(10) << process.atime << "   "
+             << setw(10) << process.btime << "   "
+             << setw(12) << waiting_time << "   "
+             << setw(12) << turnaround_time << endl;
     }
 }
 
 int main() {
     vector<Process> processes = {
-        Process(1, 0, 5),
-        Process(3, 2, 6),
-        Process(2, 1, 7)
+        Process(1, 0, 4),
+        Process(2, 1, 3),
+        Process(3, 2, 2),
+        Process(4, 3, 1)
     };
     fcfs(processes);
     return 0;
