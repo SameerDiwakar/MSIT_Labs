6dfe30a98e7b966ae1fb5262b81a597331ed0c38
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2025-11-30 22:41:33 +0530

    Create bubblesort.cpp

---
+++ DAA/bubblesort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+int arr[] = {42, 23, 15, 8, 3};
+int n = sizeof(arr) / sizeof(arr[0]);
+for (int i=0;i<n-1;i++){
+for (int j=0;j<n-i-1;j++){
+if(arr[j]>arr[j+1])swap(arr[j],arr[j+1]);
+}
+}
+for (int i = 0; i < n; i++) cout << arr[i] <<" ";
+return 0;
+}
+
+//Output:
+//3 8 15 23 42
